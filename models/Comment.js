2695bbfcb850da1fe8bf658e64fef17ba27d26a2
Polina Shishkina <158646446+polololo1212@users.noreply.github.com>
2024-07-29 23:28:40 -0400

    generateId and comments

--- models/Comment.js
+++ models/Comment.js
@@ -1,36 +1,25 @@
-const mongoose = require('mongoose')
-const {model, Schema} = require('mongoose')
+const { model, Schema} = require('mongoose')
+const mongoose = require("mongoose");
 mongoose.Schema.Types.String.set('trim', true);
 
-const userSchema = new Schema ({
+const commentSchema =
+
+    new Schema({
         _id: mongoose.Schema.Types.ObjectId,
 
         user: {
-            type: String,
-            trim: true,
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
         },
-        createdAt: {
-            type: String,
-            trim: true,
-        },
-       rating: {
-            type: String,
-            trim: true,
-       },
-      title: {
-            type: String,
-             trim: true,
 
-      },
-        description:{
-            type: [
-                {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: 'Comment'
-                }
-            ]
-        }
-    }
-)
+        createdAt: String,
 
-module.exports = model('User', userSchema)
+        rating: Number,
+
+        title: String,
+
+        description: String,
+
+    });
+
+module.exports = model('Comment', commentSchema)
