d0b785de3d97433d9895a0306a3b0baefb574a07
Polina Shishkina <158646446+polololo1212@users.noreply.github.com>
2024-07-29 23:15:15 -0400

    index changed

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-const {ApolloServer} = require ('apollo-server');
-const mongoose = require('mongoose')
+const { ApolloServer } = require('apollo-server');
+const mongoose= require('mongoose')
 const typeDefs = require('./graphql/typeDefs')
 const resolvers = require('./graphql/resolvers')
 
 const MONGODB = 'mongodb+srv://shishkinapolina02:7FQe3aiQu4VFOynC@cluster0.yds7viq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-
 const server = new ApolloServer({
     typeDefs,
     resolvers
-
 })
 
-mongoose.connect(MONGODB, {useNewUrlParser: true})
-    .then(() => {
-        console.log("MongoDB Connection successful");
-        return server.listen({port: 5000})
-    })
-    .then((res) => {
-        console.log(`Server running at ${res.url}`);
-    })
+const connectDB = async () => {
+    try {
+        await mongoose.connect(MONGODB);
+        console.log('MongoDB connected successfully');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+};
+
+connectDB().then(() => {
+
+})
+server.listen({port: 5000}).then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+});
